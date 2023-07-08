c8073247fc348910fb5a06712782242f6a6356bd
Bidur Gupta <bidurgupta125@gmail.com>
2023-07-08 23:58:51 +0530

    post

--- lib/screen/add_post_screen.dart
+++ lib/screen/add_post_screen.dart
@@ -10,7 +10,32 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff133D59),
+        title: Text(
+          "BLACKCOFFER",
+          style: TextStyle(
+              color: Colors.white
+          ),
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            iconSize: 28,
+            tooltip: 'Search',
+            onPressed: () {
+
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
 
