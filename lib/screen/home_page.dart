a340d6cf8268424ae3902cdac1e14c6dd5f7238e
Bidur Gupta <bidurgupta125@gmail.com>
2023-08-09 21:19:23 +0530

    HomePage

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -725,6 +725,102 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 10.0,
                 ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child:  Column(
+                    children: [
+                      const Row(
+                        children: [
+                          CircleAvatar(
+                            child: Icon(Icons.account_circle_rounded),
+                          ),
+                          SizedBox(width: 15.0),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "bidur_05",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16
+                                ),
+                              ),
+                              Text(
+                                "New Delhi",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 40.0),
+                            child: Text(
+                              "Morning Time",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        height: 210,
+                        child: Image(
+                          image: NetworkImage("https://th.bing.com/th/id/R.b1afe59f6c921a33b399f8430cae03cd?rik=HXoGx2F1LEfs0A&riu=http%3a%2f%2fwww.photoaxe.com%2fwp-content%2fuploads%2f2014%2f08%2fcofffecup.jpg&ehk=kjRd5mD5E19VA8q56bXzpxFGEaadsbSpZ7zSiHyHmMQ%3d&risl=&pid=ImgRaw&r=0"),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: (){}
+                              , icon: Icon(Icons.preview_sharp)
+                          ),
+                          Text(
+                            "2000",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15
+                            ),
+                          ),
+                          const SizedBox(width: 20.0,),
+                          Text(
+                            "#Adventure",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 18.0),
+                            child: Text(
+                              "2 days ago",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
